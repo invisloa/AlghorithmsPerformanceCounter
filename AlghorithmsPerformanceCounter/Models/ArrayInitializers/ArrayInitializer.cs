aef9893ee63318f5aef9a813c1b7fd1be3fe2c44
invisloa <91217120+invisloa@users.noreply.github.com>
2023-04-23 21:57:53 +0200

    InitializeMultipleArrays

--- AlghorithmsPerformanceCounter/Models/ArrayInitializers/ArrayInitializer.cs
+++ AlghorithmsPerformanceCounter/Models/ArrayInitializers/ArrayInitializer.cs
@@ -8,6 +8,9 @@ namespace AlghorithmsPerformanceCounter.Models.ArrayInitializers
 {
 	public class ArrayInitializer : IArrayInitializer
 	{
+		private const int minArraySize = 3;
+		private Random random = new Random();
+
 		public int[] SingleArrayInitializer(int numberOfValusInArray)
 		{
 			int[] array = new int[numberOfValusInArray];
@@ -17,19 +20,29 @@ namespace AlghorithmsPerformanceCounter.Models.ArrayInitializers
 			}
 			return array;
 		}
-		public int[][] InitializeMultipleArrays(int numberOfArrays, int numberOfValusInArray)
+		public int[][] InitializeMultipleArrays(int numberOfArrays, int maxNumberOfValues)
 		{
 			int[][] arrayOfArrays = new int[numberOfArrays][];
-
-			for (int i = 0; i < numberOfArrays; i++)
+			int[] baseArray = SingleArrayInitializer(maxNumberOfValues);
+			int[] firstArray = new int[minArraySize];
+			Array.Copy(baseArray, 0, firstArray, 0, minArraySize);
+			arrayOfArrays[0] = firstArray;
+			if (numberOfArrays > 2)
 			{
-				arrayOfArrays[i] = SingleArrayInitializer(numberOfValusInArray);
+				for (int i = 1; i < numberOfArrays -2; i++)
+				{
+					int currentArraySize = (maxNumberOfValues / numberOfArrays) * i;
+					int[] arrayToAdd = new int[currentArraySize];
+					Array.Copy(baseArray, 0, arrayToAdd, 0, currentArraySize);
+					arrayOfArrays[i] = arrayToAdd;
+				}
 			}
+			arrayOfArrays[numberOfArrays-1] = baseArray;
 			return arrayOfArrays;
 		}
+
 		int ValuesRandoizer()
 		{
-			Random random = new Random();
 			return random.Next(1, 1001);
 		}
 
